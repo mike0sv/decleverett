c7d76369c91fb91d1a55f6f0e457a890df64b548
mike0sv <mike0sv@gmail.com>
2021-06-02 00:08:56 +0300

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup_args = dict(
+    name='decleverett',
+    version='0.0.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='mike0sv',
+    author_email='',
+    description='',
+    install_requires=['everett'],
+    extras_require={'yaml': 'everett[yaml]'})
+
+if __name__ == '__main__':
+    setup(**setup_args)

1b0f0e94947e8e49e8f857eeced6a0a18964e4bf
mike0sv <mike0sv@gmail.com>
2021-06-02 00:28:36 +0300

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup_args = dict(
     name='decleverett',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='',
